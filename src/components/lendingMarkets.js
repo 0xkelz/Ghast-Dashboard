8c15c5aef5b0892f0a474d5a4e9695c53c7efbb1
0xkelz <101370436+0xkelz@users.noreply.github.com>
2023-07-23 20:42:12 +0200

    Add files via upload

--- src/components/lendingMarkets.js
+++ src/components/lendingMarkets.js
@@ -5,13 +5,13 @@ import WethLend from "./wethLend";
 const LendingMarkets = () => {
   return (
     <div>
-      <h1 className="text-white font-normal  text-3xl text-center mt-20 lg:text-6xl">
+      <h1 className="text-white font-normal  text-3xl text-center mt-8 lg:text-6xl lg:mt-20">
         Lending{" "}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-pink-600">
           Markets
         </span>
       </h1>
-      <div className="flex flex-wrap gap-6 justify-center mt-10 lg:gap-8">
+      <div className="flex flex-wrap gap-6 justify-center mt-6 lg:gap-8 lg:mt-10">
         <UsdcLend />
         <WethLend />
         <WbtcLend />
